1dd2ac37ba49c948957cf5ac86106bd0699dd0af
TKDKid1000 <rhoneeiler@gmail.com>
2021-03-30 09:02:58 -0700

    fixed plugin to use only fast async world edit api

--- src/main/java/net/tkdkid1000/armiworld/Home.java
+++ src/main/java/net/tkdkid1000/armiworld/Home.java
@@ -13,16 +13,16 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import com.boydti.fawe.FaweAPI;
 import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
-import com.sk89q.worldedit.world.registry.LegacyWorldData;
 
 import net.tkdkid1000.armiworld.utils.BoundingBox;
 import net.tkdkid1000.armiworld.utils.Locstring;
@@ -159,16 +159,17 @@ public class Home {
 			@Override
 			public void run() {
 				Clipboard clipboard;
-				File schem = new File("plugins"+File.separator+"WorldEdit"+File.separator+"schematics"+File.separator+schemname+".schematic");
-				ClipboardFormat format = ClipboardFormat.findByFile(schem);
+				File schem = new File("plugins"+File.separator+"FastAsyncWorldEdit"+File.separator+"schematics"+File.separator+schemname+".schem");
+				ClipboardFormat format = ClipboardFormats.findByFile(schem);
 				try {
 					ClipboardReader reader = format.getReader(new FileInputStream(schem));
-					clipboard = reader.read(LegacyWorldData.getInstance());
+					clipboard = reader.read();
 
+					@SuppressWarnings("deprecation")
 					EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(FaweAPI.getWorld(loc.getWorld().getName()), -1);
-					Operation operation = new ClipboardHolder(clipboard, LegacyWorldData.getInstance())
-				            .createPaste(editSession, LegacyWorldData.getInstance())
-				            .to(new Vector(loc.getX(), loc.getY(), loc.getZ()))
+					Operation operation = new ClipboardHolder(clipboard)
+				            .createPaste(editSession)
+				            .to(BlockVector3.at(loc.getX(), loc.getY(), loc.getZ()))
 				            .build();
 					try {
 						Operations.complete(operation);
--- src/main/java/net/tkdkid1000/armiworld/jobs/Lumberjack.java
+++ src/main/java/net/tkdkid1000/armiworld/jobs/Lumberjack.java
@@ -10,16 +10,16 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import com.boydti.fawe.FaweAPI;
 import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
-import com.sk89q.worldedit.world.registry.LegacyWorldData;
 
 import net.tkdkid1000.armiworld.ArmiWorldEconomy;
 import net.tkdkid1000.armiworld.utils.Locstring;
@@ -48,16 +48,17 @@ public class Lumberjack implements Listener {
 					@Override
 					public void run() {
 						Clipboard clipboard;
-						File schem = new File("plugins"+File.separator+"WorldEdit"+File.separator+"schematics"+File.separator+ArmiWorldEconomy.getInstance().config.getString("jobs.lumberjack.schematic")+".schematic");
-						ClipboardFormat format = ClipboardFormat.findByFile(schem);
+						File schem = new File("plugins"+File.separator+"FastAsyncWorldEdit"+File.separator+"schematics"+File.separator+ArmiWorldEconomy.getInstance().config.getString("jobs.lumberjack.schematic")+".schem");
+						ClipboardFormat format = ClipboardFormats.findByFile(schem);
 						try {
 							ClipboardReader reader = format.getReader(new FileInputStream(schem));
-							clipboard = reader.read(LegacyWorldData.getInstance());
-							
+							clipboard = reader.read();
+
+							@SuppressWarnings("deprecation")
 							EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(FaweAPI.getWorld(loc.getWorld().getName()), -1);
-							Operation operation = new ClipboardHolder(clipboard, LegacyWorldData.getInstance())
-						            .createPaste(editSession, LegacyWorldData.getInstance())
-						            .to(new Vector(loc.getX(), loc.getY(), loc.getZ()))
+							Operation operation = new ClipboardHolder(clipboard)
+						            .createPaste(editSession)
+						            .to(BlockVector3.at(loc.getX(), loc.getY(), loc.getZ()))
 						            .build();
 							try {
 								Operations.complete(operation);
